f2bac92610cf2598044ecb079a3cfcc9c2a855ba
Yusuke Kuga <s24c1047uj@s.chibakoudai.jp>
2025-11-06 16:13:40 +0900

    modifie test.bash

--- test.bash
+++ test.bash
@@ -1,14 +1,16 @@
-#!/bin/bash 
-#SPDX
-n () {
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Yusuke Kuga 　　　　　
+# SPDX-License-Identifier: GPL-3.0-only
+
+ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
 
-a=$(seq 5 | ./plus)
-[ "${a}" = 15 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINEMO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
